7df7413eeaf621d6e89280aa67b6af120ae0ee3c
marcoweinmann <marco.wm94@gmail.com>
2022-07-07 16:15:10 +0200

    fertig bis mutableList

--- app/src/main/java/com/example/telefonbuch/MainActivity.kt
+++ app/src/main/java/com/example/telefonbuch/MainActivity.kt
@@ -1,7 +1,16 @@
 package com.example.telefonbuch
 
+import android.content.Context
+import android.graphics.Insets.add
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.core.graphics.Insets.add
+import androidx.core.view.OneShotPreDrawListener.add
+import com.example.telefonbuch.adapter.ItemAdapter
+import com.example.telefonbuch.data.Datasource
+import com.example.telefonbuch.data.model.Contact
 import com.example.telefonbuch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +20,32 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+    val contacts = Datasource().loadContacts()
+
+        binding.TBRecycler.adapter = ItemAdapter(this, contacts)
+        binding.buttonHinzufGen.setOnClickListener {
+        val name = binding.EditTextName.text.toString()
+        val nummer = binding.EditTextTelefonnummer.text.toString()
+
+
+
+            val newName = binding.EditTextName.text.toString()
+            val newNumber = binding.EditTextTelefonnummer.text.toString()
+
+            if (newNumber != "") {
+                val newContact = Contact(newNumber, newName)
+                contacts.add(0, newContact)
+                binding.TBRecycler.adapter?.notifyItemInserted(0)
+                binding.EditTextName.setText("")
+                Toast.makeText(this, "Contact has been added", Toast.LENGTH_SHORT).show()
+            }
+
+            val imm = this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+
+        }
+
     }
 }
--- app/src/main/java/com/example/telefonbuch/adapter/itemAdapter.kt
+++ app/src/main/java/com/example/telefonbuch/adapter/itemAdapter.kt
@@ -1,4 +1,50 @@
 package com.example.telefonbuch.adapter
 
-class itemAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.view.menu.MenuView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.telefonbuch.R
+import com.example.telefonbuch.data.model.Contact
+
+class ItemAdapter (
+    private val context:Context,
+    private val dataset: List<Contact>
+
+        ) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>(){
+
+            class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+                val textViewName: TextView = itemView.findViewById(R.id.textViewName)
+                val textViewNumber: TextView = itemView.findViewById(R.id.textViewTelefonnummer)
+
+            }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder{
+
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item, parent, false)
+
+        return ItemViewHolder(adapterLayout)
+
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int){
+        val item = dataset[position]
+        holder.textViewName.text = item.name
+        holder.textViewNumber.text = item.nummer
+
+    }
+
+    override fun getItemCount(): Int {
+        return dataset.size
+
+    }
+
 }
+
+
+
+
